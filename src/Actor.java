30af9816f3258982428e69fdc1c2acdac09f1b07
Michael Chacko <mikechacko10@gmail.com>
2019-10-13 22:48:19 -0500

    taking too long

--- src/Actor.java
+++ src/Actor.java
@@ -1,5 +1,5 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Actor {
     //identifying characteristics
@@ -8,16 +8,35 @@ public class Actor {
     public String character_name = null;
 
     //similarity qualifiers
-    public int numberOfMovies;
+    public HashSet<String> directorsWorkedWith;
+    public HashSet<Movie> moviesActedIn;
+    public HashSet<Actor> actorsWorkedWith;
     public String mostCommonGenre;
-    public ArrayList<String> directorsWorkedWith;
-    public ArrayList<Actor> actorsWorkedWith;
-    //also acted in movies with
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Actor actor = (Actor) o;
+        return castid == actor.castid;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(castid);
+    }
 
     public Actor(int id, String name, String character){
+        directorsWorkedWith = new HashSet<String>();
+        moviesActedIn = new HashSet<Movie>();
+        actorsWorkedWith = new HashSet<Actor>();
+
         this.castid = id;
         this.name = name;
         this.character_name = character;
+    }
+
+    public void print() {
+        System.out.println("castid " + this.castid + ": " + this.name);
     }
 }
--- src/SimilarActors.java
+++ src/SimilarActors.java
@@ -9,34 +9,68 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashSet;
 
 public class SimilarActors {
     private String baseFilename;
     private Integer query;
     private Connection conn;
     private Statement search;
+    private HashMap<Integer, ArrayList<String>> genres;
 
     public SimilarActors(Connection conn){
         if(conn == null) {
             System.out.println("SimilarActors: Connection is bad.");
         }
         this.conn = conn;
+
+        try {
+            genres = new HashMap<Integer, ArrayList<String>>();
+            getAllGenres();
+        }
+        catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void getAllGenres() throws java.sql.SQLException{
+        String sqlQuery = "SELECT * FROM genres;";
+
+        ResultSet rs = database_search(sqlQuery);
+
+        while (rs.next()) {
+            int movie_id = rs.getInt("movie_id");
+            String genre_name = rs.getString("name");
+            ArrayList<String> list = genres.get(movie_id);
+            if(list == null){
+                list = new ArrayList<String>();
+                list.add(genre_name);
+                genres.put(movie_id, list);
+            } else {
+                list.add(genre_name);
+            }
+        }
     }
 
     //given two movies, print two actors who are most similar and give the reasons
     void compute(String movie1, String movie2) throws java.sql.SQLException {
 
-        ArrayList<Actor> actorsInMovie1 = getActorsInMovie(movie1);
-        ArrayList<Actor> actorsInMovie2 = getActorsInMovie(movie2);
+        HashSet<Actor> actorsInMovie1 = getActorsInMovie(movie1);
+        HashSet<Actor> actorsInMovie2 = getActorsInMovie(movie2);
 
         System.out.println("Actor in movie 1");
         for(Actor a : actorsInMovie1){
-            printActorName(a);
+            System.out.println("Computing Stats for: " + a.name);
+            computeActorStats(a);
+            a.print();
         }
 
         System.out.println("Actor in movie 2");
         for(Actor a : actorsInMovie2){
-            printActorName(a);
+            System.out.println("Computing Stats for: " + a.name);
+            computeActorStats(a);
+            a.print();
         }
 
         System.out.println("Actor from movie 1: ");
@@ -44,12 +78,9 @@ public class SimilarActors {
         System.out.println("Reasons: ");
     }
 
-    void printActorName(Actor a) {
-        System.out.println("castid " + a.castid + ": " + a.name);
-    }
-
     //Returns a list of Actors who act in 'movie'
-    ArrayList<Actor> getActorsInMovie(String movie) throws java.sql.SQLException {
+    HashSet<Actor> getActorsInMovie(String movie) throws java.sql.SQLException {
+        //try to just access the actor id through character table.
         String sqlQuery =
                 "SELECT c.castid AS castid, \"cast\".name AS actor_name, c.name AS character_name " +
                 "FROM characters AS c " +
@@ -62,7 +93,7 @@ public class SimilarActors {
         sqlQuery = sqlQuery.replaceAll("REPLACEME", movie);
 
         ResultSet rs = database_search(sqlQuery);
-        ArrayList<Actor> list = new ArrayList<Actor>();
+        HashSet<Actor> list = new HashSet<Actor>();
 
         while (rs.next()) {
             int castId = rs.getInt("castid");
@@ -74,6 +105,78 @@ public class SimilarActors {
         }
 
         return list;
+    }
+
+    //Returns a list of Actors who act in 'movie'
+    HashSet<Actor> getActorsInMovie(int movieid) throws java.sql.SQLException {
+        String sqlQuery =
+                "SELECT c.castid AS castid, \"cast\".name AS actor_name, c.name AS character_name " +
+                        "FROM characters AS c " +
+                        "INNER JOIN (SELECT id from movie1 WHERE id = 'REPLACEME') AS m " +
+                        "ON c.movieid = m.id " +
+                        "INNER JOIN \"cast\" " +
+                        "ON \"cast\".id = c.castid " +
+                        "WHERE c.iscrew = false;";
+
+        sqlQuery = sqlQuery.replaceAll("REPLACEME", Integer.toString(movieid));
+
+        ResultSet rs = database_search(sqlQuery);
+        HashSet<Actor> list = new HashSet<Actor>();
+
+        while (rs.next()) {
+            int castId = rs.getInt("castid");
+            String actor_name = rs.getString("actor_name");
+            String character_name = rs.getString("character_name");
+
+            Actor a = new Actor(castId, actor_name, character_name);
+            list.add(a);
+        }
+
+        return list;
+    }
+
+    private void computeActorStats(Actor actor) throws java.sql.SQLException{
+        //Movies Acted In
+        String sqlQuery = "SELECT m.id AS movieid, m.original_title AS movie_title " +
+                "FROM movie1 as m " +
+                "INNER JOIN characters AS c " +
+                "ON c.movieid = m.id " +
+                "WHERE castid = REPLACEME;";
+        sqlQuery = sqlQuery.replaceAll("REPLACEME", Integer.toString(actor.castid));
+
+        ResultSet rs = database_search(sqlQuery);
+
+        while (rs.next()) {
+            int movieId = rs.getInt("movieid");
+            String movie_name = rs.getString("movie_title");
+
+            Movie m = new Movie(movieId, movie_name);
+
+
+            actor.moviesActedIn.add(m);
+        }
+
+        for(Movie m : actor.moviesActedIn){
+            m.genre = genres.get(m.id);
+            /*
+            //get movie genres
+            sqlQuery = "SELECT name AS genre FROM genres WHERE movie_id = " + Integer.toString(m.id) + ";";
+
+            ResultSet rs2 = database_search(sqlQuery);
+            while(rs2.next()){
+                String genre = rs2.getString(1);
+                m.genre.add(genre);
+
+            }
+            */
+        }
+
+        /*
+        for(Movie m : actor.moviesActedIn){
+            actor.actorsWorkedWith.addAll(getActorsInMovie(m.id));
+        }
+        */
+        System.out.println("done");
     }
 
     private ResultSet database_search(String sqlQuery) throws java.sql.SQLException {
